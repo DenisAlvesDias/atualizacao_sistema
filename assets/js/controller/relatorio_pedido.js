2e6ef807b3f7afed47a638d1e506b396f753dd29
DenisAlvesDias <denis.al.dias@gmail.com>
2023-09-28 10:44:06 -0300

    Delete assets directory

--- assets/js/controller/relatorio_pedido.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('app', [])
-
-app.controller('relatorio_pedido', function($scope, $http, $window, $timeout) {
-  
-        $scope.relatorio = angular.fromJson(localStorage.getItem("relatorio"));
-      
-   
-    })
